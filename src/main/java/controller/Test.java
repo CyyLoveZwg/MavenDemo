50d48cea09aca69db0048b313ed1babd1332f1c8
zwg <13431812786@163.com>
2018-02-02 15:21:43 +0800

    测试哈哈哈

---
+++ src/main/java/controller/Test.java
@@ -0,0 +1,10 @@
+package controller;
+
+/**
+ * Created by zwg on 2018/2/2 15:18
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈");
+    }
+}
